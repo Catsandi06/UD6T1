855ca7fcf7d810e73c1f50b3335539e9867a0412
Nichel Morales <usr3742@salesianos-lacuesta.net>
2026-04-30 21:05:01 +0100

    Crear clase Reparacion con atributos y metodos basicos

---
+++ src/net/salesianos/reparacion/Reparacion.java
@@ -0,0 +1,24 @@
+package net.salesianos.reparacion;
+
+public class Reparacion {
+    private String codigo;
+    private String cliente;
+    private String descripcion;
+    private boolean urgente;
+
+    public Reparacion(String codigo, String cliente, String descripcion, boolean urgente) {
+        this.codigo = codigo;
+        this.cliente = cliente;
+        this.descripcion = descripcion;
+        this.urgente = urgente;
+    }
+
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public String getCliente() { return cliente; }
+    public void setCliente(String cliente) { this.cliente = cliente; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public boolean isUrgente() { return urgente; }
+    public void setUrgente(boolean urgente) { this.urgente = urgente; }
+}
