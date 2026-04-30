029c5b1c87aec5d496a9f4e2c46d48df34b71905
Nichel Morales <usr3742@salesianos-lacuesta.net>
2026-04-30 21:06:11 +0100

    Anadir mas reparaciones a la cola

--- src/net/salesianos/App.java
+++ src/net/salesianos/App.java
@@ -13,6 +13,8 @@ public class App {
         Queue<Reparacion> cola = new ArrayDeque<>();
         cola.add(new Reparacion("R001", "Ana", "Cambio de pantalla", false));
         cola.add(new Reparacion("R002", "Luis", "Bateria danada", true));
+        cola.add(new Reparacion("R003", "Marta", "Teclado roto", false));
+        cola.add(new Reparacion("R004", "Pedro", "Puerto de carga", true));
         System.out.println("Reparaciones anadidas a la cola");
     }
 }
